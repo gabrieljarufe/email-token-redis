2b2417a20ad8d92afc09f4209febf94e83b29e37
Gabriel Alves Jarufe <gabrieljarufe1@gmail.com>
2024-03-24 15:04:48 -0300

    adding `README.md` and initial sping boot files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Email Token Solution using Redis
+
+Email Token Sender and Validates using Redis Key-Value Storage
+
+## Tutorial
+
+This is the **P**roof **O**f **C**oncept (**POC**) of a solution that will be part of a project called [YaruChat](https://github.com/gabrieljarufe/yarufzap)
+Tutorial soon.
---
+++ src/main/java/com/jarufe/emailtokenredis/EmailTokenRedisApplication.java
@@ -0,0 +1,13 @@
+package com.jarufe.emailtokenredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailTokenRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailTokenRedisApplication.class, args);
+	}
+
+}
